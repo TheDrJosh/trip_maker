489a536c393bb4931d81fdfae547b8358a72e813
TheDrJosh <josh.a.roo2004@gmail.com>
2026-01-29 21:18:53 -0800

    basics test 11

--- src/routes/root/partials/generate.rs
+++ src/routes/root/partials/generate.rs
@@ -87,7 +87,7 @@ fn location_card(location: &random_location::LocationInfo, distance_unit: Distan
             }
 
             div class="fixed inset-0 bg-black/50 w-full h-full flex items-center justify-center" x-show="open" x-cloak {
-                div class="bg-zinc-800 rounded-xl p-4 max-w-lg w-full" "x-on:click.outside"="open = false" {
+                div class="bg-zinc-600 rounded-xl p-4 max-w-lg w-full" "x-on:click.outside"="open = false" {
                     h2 class="text-2xl font-bold mb-4" { ( &location.name ) }
                     p {
                         "Rating: " ( location.rating )
@@ -100,7 +100,7 @@ fn location_card(location: &random_location::LocationInfo, distance_unit: Distan
                     }
                     p { ( &location.address ) }
                     @if let Some(description) = &location.description {
-                        p { ( description ) }
+                        p class="pt-2" { ( description ) }
                     }
                     button class="bg-zinc-700 px-2 py-1 rounded-lg hover:bg-zinc-800 mt-4" x-on:click="open = false" {
                         "Close"
