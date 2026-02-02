33881ce872d45b01272a2d90def37e370a2f3f38
TheDrJosh <josh.a.roo2004@gmail.com>
2026-02-02 15:53:18 -0800

    working auth build 2

--- src/auth/db.ts
+++ src/auth/db.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "../db/schema";
-import { env } from "./env";
+import * as schema from "../db/schema.ts";
+import { env } from "./env.ts";
 
 export const db = drizzle(env.DATABASE_URL, { schema });
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -5,40 +5,43 @@ import { MemoryStorage } from "@openauthjs/openauth/storage/memory";
 import { PasswordUI } from "@openauthjs/openauth/ui/password";
 import { eq } from "drizzle-orm";
 import { generateFromEmail } from "unique-username-generator";
-import { users } from "../db/schema";
-import { subjects } from "../lib/auth/sesstion";
-import { db } from "./db";
-import { env } from "./env";
+import { users } from "../db/schema.ts";
+import { subjects } from "../lib/auth/sesstion.ts";
+import { db } from "./db.ts";
+import { env } from "./env.ts";
 
 // import { StorageAdapter } from "@openauthjs/openauth/storage/storage";
 
 async function getUser(email: string) {
     const db_user = await db.select().from(users).where(eq(users.email, email));
 
-    if (db_user.length === 0) {
-        const username = generateFromEmail(email);
-
-        const created_user = (
-            await db
-                .insert(users)
-                .values({
-                    email: email,
-                    username: username,
-                })
-                .returning()
-        )[0];
-
+    if (db_user[0]) {
         return {
-            id: created_user.id,
-            email: created_user.email,
-            username: created_user.username,
+            id: db_user[0].id,
+            email: db_user[0].email,
+            username: db_user[0].username,
         };
+    }
+    const username = generateFromEmail(email);
+
+    const created_user = (
+        await db
+            .insert(users)
+            .values({
+                email: email,
+                username: username,
+            })
+            .returning()
+    )[0];
+
+    if (!created_user) {
+        throw new Error("Failed to create user");
     }
 
     return {
-        id: db_user[0].id,
-        email: db_user[0].email,
-        username: db_user[0].username,
+        id: created_user.id,
+        email: created_user.email,
+        username: created_user.username,
     };
 }
 
